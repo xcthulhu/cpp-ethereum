c1ad63e33315fc973730ae427e86cdf3eb885f1a
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-01-07 12:40:35 -0700

    No longer writing to log file.

--- libdaggerhashimoto/benchmark.c
+++ libdaggerhashimoto/benchmark.c
@@ -11,26 +11,25 @@ int main(void){
   const int
     size = 100000,
     trials = 100;
+  unsigned int hash_bit = 0;
   num dag[size], nonce = num_zero;
   clock_t begin, end;
   double time_spent;
-  FILE *log = fopen("out.log", "w");
   char out[155];
   parameters params = get_default_params();
 
   params.n = size;
   produce_dag(dag, params, seed);
-  for (int accesses = 1; accesses < 1000 ; ++accesses) {
+  for (int accesses = 1; accesses < 2000 ; ++accesses) {
     params.accesses = accesses;
     begin = clock();
     for (int i = 0 ; i < trials ; i++) {
       num_add(nonce, num_one);
-      write_num(out, hashimoto(dag, params, header, nonce));
-      fprintf(log,"%s\n",out);
+      hash_bit ^= num_to_uint(hashimoto(dag, params, header, nonce)) & 1;
     }
     end = clock();
     time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("%i\t%f\n", accesses, time_spent);
+    printf("%i\t%f\t%d\n", accesses, time_spent, hash_bit);
   }
   return EXIT_SUCCESS;
 }
